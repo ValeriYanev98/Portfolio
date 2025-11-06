10b7e962d0c018b6bd541e258e700888813d37e9
Valery <valeri.t.yanev@gmail.com>
2025-11-06 12:41:50 +0200

    using resend instead of nodemailer, the main reason is that nodemailer uses 587 port, which is blocked by Railway, Resend uses 443 port which is not blocked

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -1,35 +1,19 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 import * as dotenv from "dotenv";
 import { Email } from "custom-validation";
 
 dotenv.config();
 
-const GMAIL_USER = process.env.GMAIL_USER;
-const GMAIL_APP_PASSWORD = process.env.GMAIL_APP_PASSWORD;
+const EMAIL = process.env.EMAIL as string;
+const RESEND_API_KEY = process.env.RESEND_API_KEY as string;
+
+const resend = new Resend(RESEND_API_KEY);
 
 export const sendEmailService = (data: Email) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: GMAIL_USER,
-      pass: GMAIL_APP_PASSWORD,
-    },
-  });
-
-  const mailOptions = {
+  resend.emails.send({
     from: data.email,
-    to: GMAIL_USER,
-    subject: "Hello",
-    text: "HI there!",
-  };
-
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      console.error("Error sending email: ", err);
-    } else {
-      console.log("Email sent: ", info.response);
-    }
+    to: EMAIL,
+    subject: "Hello World!",
+    html: `HI there`,
   });
 };
