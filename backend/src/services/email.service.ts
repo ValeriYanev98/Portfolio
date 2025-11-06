a36ff5cb2b368159528e323d7eb2178568ea10b7
Valery <valeri.t.yanev@gmail.com>
2025-11-06 17:27:20 +0200

    email are environmental variables now

--- backend/src/services/email.service.ts
+++ backend/src/services/email.service.ts
@@ -4,15 +4,16 @@ import { Email } from "custom-validation";
 
 dotenv.config();
 
-const EMAIL = process.env.EMAIL as string;
+const EMAIL_FROM = process.env.EMAIL_TO as string;
+const EMAIL_TO = process.env.EMAIL_FROM as string;
 const RESEND_API_KEY = process.env.RESEND_API_KEY as string;
 
 const resend = new Resend(RESEND_API_KEY);
 
 export const sendEmailService = async (email: Email) => {
-  const { data, error } = await resend.emails.send({
-    from: "Portfolio <no-reply@valeriyanev.com>",
-    to: "valeri.t.yanev@gmail.com",
+  const { error } = await resend.emails.send({
+    from: `Portfolio ${EMAIL_FROM}>`,
+    to: EMAIL_TO,
     subject: `Reach out from - ${email.email}`,
     html: `
   <div style="font-family: Arial, sans-serif; color: #222; line-height: 1.5;">
@@ -24,6 +25,6 @@ export const sendEmailService = async (email: Email) => {
   });
 
   if (error) {
-    throw new Error("Failed to send email!");
+    console.log("Failed to send email!");
   }
 };
