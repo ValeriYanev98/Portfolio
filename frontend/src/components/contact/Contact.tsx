1bcdaedb03abe7762aeaead18e5e60eacf315638
Valery <valeri.t.yanev@gmail.com>
2025-11-06 18:19:53 +0200

    added both detection , to break submition if its too fast or botField is populated.

--- frontend/src/components/contact/Contact.tsx
+++ frontend/src/components/contact/Contact.tsx
@@ -4,26 +4,34 @@ import { useForm } from "react-hook-form";
 import type { SubmitHandler } from "react-hook-form";
 import { EmailSchema } from "custom-validation";
 import type { Email } from "custom-validation";
-import axios from "axios";
+import axios, { type AxiosResponse } from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
 
 const Contact: React.FC = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useForm<Email>({ resolver: zodResolver(EmailSchema), reValidateMode: "onBlur" });
 
-  const sendEmail: SubmitHandler<Email> = async (data): Promise<void> => {
-    console.log(data);
+  useEffect(() => {
+    setValue("loadTime", Date.now()); // Set the initial load time when the form mounts.
+  }, [setValue]);
 
-    const response = await axios.post(
+  const sendEmail: SubmitHandler<Email> = async (data): Promise<void> => {
+    const response: AxiosResponse<{ success: true }> = await axios.post(
       "https://api-portfolio.valeriyanev.com/send-email",
       { data },
       { headers: { "Content-Type": "application/json" } }
     );
 
-    console.log(response);
+    if (response.data.success) {
+      setValue("email", "");
+      setValue("name", "");
+      setValue("message", "");
+    }
   };
 
   return (
@@ -34,19 +42,22 @@ const Contact: React.FC = () => {
       <form onSubmit={handleSubmit(sendEmail)}>
         <div className={styles.sectionContactRowWrapper}>
           <label>Name: </label>
-          <input {...register("name")} type="text" />
+          <input {...register("name")} type="text" placeholder="Name..." />
           {errors && errors.name && <span>{errors.name.message}</span>}
         </div>
         <div className={styles.sectionContactRowWrapper}>
           <label>Email: </label>
-          <input {...register("email")} type="text" />
+          <input {...register("email")} type="email" placeholder="Email..." />
           {errors && errors.email && <span>{errors.email.message}</span>}
         </div>
         <div className={styles.sectionContactRowWrapper}>
           <label>Message: </label>
-          <textarea {...register("message")} cols={50} rows={8} />
+          <textarea {...register("message")} cols={50} rows={8} placeholder="Message..." />
           {errors && errors.message && <span>{errors.message.message}</span>}
         </div>
+
+        <input {...register("botField")} autoComplete="off" style={{ display: "none" }} tabIndex={-1} />
+        <input type="hidden" {...register("loadTime", { valueAsNumber: true })} />
 
         <button type="submit">Send Email</button>
       </form>
