9beb875490bca45ec15e97ee3a435a2054033d09
Valery <valeri.t.yanev@gmail.com>
2025-11-06 18:19:53 +0200

    added both detection , to break submition if its too fast or botField is populated.

--- packages/custom-validation/src/schemas/email.ts
+++ packages/custom-validation/src/schemas/email.ts
@@ -12,4 +12,6 @@ export const EmailSchema = z.object({
     .regex(/^[\x20-\x7A]*$/, { error: "Only normal characters!" })
     .min(20, { error: "Message too short, expected at least 20 characters." })
     .max(1000, { error: "Message too long!" }),
+  botField: z.string().max(0, { error: "Nice try, BOT!" }), // honeypot check if there is bot trying to spam, he will try to fill all fields.
+  loadTime: z.number().refine((val) => Date.now() - val > 3000), // if the form was submitted too fast, like less than 3 seconds, def a bot, since the textarea doesnt have autofill option.
 });
