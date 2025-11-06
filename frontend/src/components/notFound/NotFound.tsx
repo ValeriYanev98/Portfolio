a37bb543a8d8dd40c3febb633ae1b8f62e7c3780
Valery <valeri.t.yanev@gmail.com>
2025-11-06 18:00:13 +0200

    added countdown timer to show the user when he will be redirected

--- frontend/src/components/notFound/NotFound.tsx
+++ frontend/src/components/notFound/NotFound.tsx
@@ -1,29 +1,32 @@
 import type React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import styles from "./NotFound.module.scss";
 
 const NotFound: React.FC = () => {
+  const [seconds, setSeconds] = useState(4);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       window.location.href = "/";
-    }, 5000);
+    }, 4000);
 
     return () => clearTimeout(timer);
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((s) => (s > 0 ? s - 1 : 0));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div
-      style={{
-        textAlign: "center",
-        padding: "4rem",
-        fontFamily: "system-ui, sans-serif",
-      }}
-    >
-      <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>404</h1>
+    <div className={styles.notFound}>
+      <h1>404</h1>
       <p>Oops! That page doesn’t exist.</p>
-      <p>Redirecting you back home in a few seconds...</p>
-      <a href="/" style={{ color: "#0070f3", textDecoration: "underline" }}>
-        Click here if it doesn’t happen automatically
-      </a>
+      <p>Redirecting you back home in {seconds > 1 ? `${seconds} seconds...` : `1 second...`}</p>
+      <a href="/">Click here if it doesn’t happen automatically</a>
     </div>
   );
 };
