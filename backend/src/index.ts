c92ce0217e7fad657ec538ed77b88c0745294738
Valery <valeri.t.yanev@gmail.com>
2025-11-06 10:28:22 +0200

    small express app for handling email sending

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { mailRouter } from "./routes/email.route.js";
+import cors from "cors";
+import { limiter } from "./utils/rateLimiter.js";
+import helmet from "helmet";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set("trust proxy", 1); // Reads the correct client IP address.
+
+app.use(helmet());
+app.use(cors({ origin: ["https://www.valeriyanev.com", "localhost:5173"], optionsSuccessStatus: 200 }));
+app.use(express.json());
+app.use(limiter);
+
+app.use("/send-email", mailRouter);
+
+app.get("/", (req, res) => res.status(200).json({ Hello: "world" }));
+
+app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
