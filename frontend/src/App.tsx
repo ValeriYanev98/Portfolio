d24a12802745b4180b4846db841d7a28e27ee4d0
Valery <valeri.t.yanev@gmail.com>
2025-11-07 20:28:34 +0200

    removed the type from the import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import type React from "react";
+import React from "react";
 import Layout from "./components/layout/Layout";
 import Hero from "./components/hero/Hero";
 import Projects from "./components/projects/Projects";
--- frontend/src/components/layout/header/Header.tsx
+++ frontend/src/components/layout/header/Header.tsx
@@ -1,4 +1,4 @@
-import type React from "react";
+import React from "react";
 import Navbar from "../navbar/Navbar";
 import styles from "./Header.module.scss";
 
