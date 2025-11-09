6756453d9125eb74dc55d3eb12726e5cf34d741b
Valery <valeri.t.yanev@gmail.com>
2025-11-09 14:13:42 +0200

    added images for the different techs

--- frontend/src/types/projects.ts
+++ frontend/src/types/projects.ts
@@ -1,18 +1,32 @@
-type FrontendCore = "React" | "JavaScript" | "TypeScript";
-type FrontendStyling = "SASS/SCSS" | "Bootstrap" | "CSS";
-type FrontendTools = "Vite" | "Redux Toolkit" | "Context API";
+type FrontendCore =
+  | { text: "React"; img: "svg/react.svg" }
+  | { text: "JavaScript"; img: "svg/javascript.svg" }
+  | { text: "TypeScript"; img: "svg/typescript.svg" }
+  | { text: "HTML"; img: "svg/html5.svg" };
+type FrontendStyling =
+  | { text: "SASS/SCSS"; img: "svg/sass.svg" }
+  | { text: "Bootstrap"; img: "svg/bootstrap.svg" }
+  | { text: "CSS"; img: "svg/css.svg" };
+type FrontendTools =
+  | { text: "Vite"; img: "svg/vite.svg" }
+  | { text: "Redux Toolkit"; img: "svg/redux.svg" }
+  | { text: "Context API"; img: "svg/react.svg" };
 
-type Server = "Node.js" | "Express.js";
-type Database = "PostgreSQL" | "MongoDB";
-type Integration = "Stripe API";
+type Server = { text: "Node.js"; img: "svg/nodejs.svg" } | { text: "Express.js"; img: "svg/express.svg" };
+type Database =
+  | { text: "PostgreSQL"; img: "svg/postgresql.svg" }
+  | { text: "MongoDB"; img: "svg/mongodb.svg" }
+  | { text: "Mongoose"; img: "svg/mongoose.svg" };
 
-type VersionControl = "Git" | "GitHub";
-type Testing = "Vitest" | "React Testing Library";
-type Deployment = "Vercel";
+type VersionControl = { text: "GitHub"; img: "svg/github.svg" };
+type Testing =
+  | { text: "Vitest"; img: "svg/vitest.svg" }
+  | { text: "React Testing Library"; img: "svg/testinglibrary.svg" };
+type Deployment = { text: "Vercel"; img: "svg/vercel.svg" };
 
 export type TechMap = {
   frontend: { core: FrontendCore[]; styling: FrontendStyling[]; tooling: FrontendTools[] };
-  backend: { server: Server[]; database: Database[]; integration: Integration[] };
+  backend?: { server: Server[]; database: Database[] };
   development: { versionControl: VersionControl[]; testing: Testing[]; deployment: Deployment[] };
 };
 
