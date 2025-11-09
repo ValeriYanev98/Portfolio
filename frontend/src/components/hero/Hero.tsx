68eb9410e5a6543613e31f936bb8bdecff049e12
Valery <valeri.t.yanev@gmail.com>
2025-11-09 21:27:13 +0200

    added id to scroll to those sections when clicked the link on the navbar

--- frontend/src/components/hero/Hero.tsx
+++ frontend/src/components/hero/Hero.tsx
@@ -3,7 +3,7 @@ import styles from "./Hero.module.scss";
 
 const Hero: React.FC = () => {
   return (
-    <section className={styles.sectionHero}>
+    <section className={styles.sectionHero} id="hero">
       <h1 className={styles.heading}>Valeri Yanev | Full-Stack Web Developer</h1>
       <p className={styles.introduction}>
         I build secure and fast web applications with React, TypeScript and Node.js, creating complex features like
