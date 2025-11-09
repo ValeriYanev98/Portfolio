a6e6e11a587c898d25e98c94fde0d4340099ce8b
Valery <valeri.t.yanev@gmail.com>
2025-11-09 14:14:19 +0200

    exported logic to custom component

--- frontend/src/components/projects/Projects.tsx
+++ frontend/src/components/projects/Projects.tsx
@@ -1,13 +1,12 @@
-import { useEffect, useRef, useState } from "react";
-import type React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import type { ProjectsInterface } from "../../types/projects";
 import styles from "./Projects.module.scss";
 import { PROJECTS } from "../../data/projects";
 import SingleProject from "./SingleProject";
+import SelectedProject from "./SelectedProject";
 
 const Projects: React.FC = () => {
   const [selectedProject, setSelectedProject] = useState<null | ProjectsInterface>(null);
-  const [isLoadedImage, setIsLoadedImage] = useState(false);
 
   const blurredOverlayRef = useRef<HTMLDivElement>(null);
 
@@ -28,20 +27,6 @@ const Projects: React.FC = () => {
     };
   }, []);
 
-  // Wait for the image to fully load, since there might be some lag in the CSS animation.
-  useEffect(() => {
-    if (selectedProject) {
-      const image = new Image();
-
-      image.src = `/${selectedProject.image}`;
-      image.onload = () => {
-        setIsLoadedImage(true);
-      };
-    } else {
-      setIsLoadedImage(false);
-    }
-  }, [selectedProject]);
-
   // Close the selected project if clicked outside of it.
   useEffect(() => {
     const clickOutsideSelectedProject = (e: PointerEvent) => {
@@ -55,139 +40,12 @@ const Projects: React.FC = () => {
     return () => removeEventListener("click", clickOutsideSelectedProject);
   }, []);
 
-  console.log(selectedProject);
-
   return (
     <section className={styles.sectionProjects}>
-      {selectedProject && isLoadedImage && (
-        <div className={styles.selectedProject}>
-          <h3>{selectedProject.overview}</h3>
-
-          <div className={styles.selectedProjectMainPart}>
-            <a href={selectedProject.url} target="_blank">
-              <video src={`/${selectedProject.video}`} className={styles.selectedProjectVideo} muted autoPlay loop />
-            </a>
-
-            <div className={styles.selectedProjectInformation}>
-              <p>Information:</p>
-
-              {selectedProject.tech.frontend && (
-                <>
-                  <p className={styles.selectedProjectFeBeDe}>Frontend:</p>
-
-                  <div className={styles.selectedProjectTechWrapper}>
-                    <span>Core: </span>
-
-                    {selectedProject.tech.frontend.core.map((tech, i) => (
-                      <span key={tech} className={styles.selectedProjectTech}>
-                        {tech}
-                        {i < selectedProject.tech.frontend.core.length - 1 && ", "}
-                      </span>
-                    ))}
-                  </div>
-
-                  <div className={styles.selectedProjectTechWrapper}>
-                    <span>Styling: </span>
-
-                    {selectedProject.tech.frontend.styling.map((tech, i) => (
-                      <span key={tech} className={styles.selectedProjectTech}>
-                        {tech}
-                        {i < selectedProject.tech.frontend.styling.length - 1 && ", "}
-                      </span>
-                    ))}
-                  </div>
-                  <div className={styles.selectedProjectTechWrapper}>
-                    <span>Tools: </span>
-
-                    {selectedProject.tech.frontend.tooling.map((tech, i) => (
-                      <span key={tech} className={styles.selectedProjectTech}>
-                        {tech}
-                        {i < selectedProject.tech.frontend.tooling.length - 1 && ", "}
-                      </span>
-                    ))}
-                  </div>
-                </>
-              )}
-
-              {selectedProject.tech.backend && (
-                <>
-                  <p className={styles.selectedProjectFeBeDe}>Backend:</p>
-
-                  <div className={styles.selectedProjectTechWrapper}>
-                    <span>Server: </span>
-
-                    {selectedProject.tech.backend.server.map((tech, i) => (
-                      <span key={tech} className={styles.selectedProjectTech}>
-                        {tech}
-                        {i < selectedProject.tech.backend.server.length - 1 && ", "}
-                      </span>
-                    ))}
-                  </div>
-
-                  <div className={styles.selectedProjectTechWrapper}>
-                    <span>Database: </span>
-
-                    {selectedProject.tech.backend.database.map((tech, i) => (
-                      <span key={tech} className={styles.selectedProjectTech}>
-                        {tech}
-                        {i < selectedProject.tech.backend.database.length - 1 && ", "}
-                      </span>
-                    ))}
-                  </div>
-                </>
-              )}
-
-              {selectedProject.tech.development && (
-                <>
-                  <p className={styles.selectedProjectFeBeDe}>Development:</p>
-
-                  <div className={styles.selectedProjectTechWrapper}>
-                    <span>Version Control: </span>
-
-                    {selectedProject.tech.development.versionControl.map((tech, i) => (
-                      <span key={tech} className={styles.selectedProjectTech}>
-                        {tech}
-                        {i < selectedProject.tech.development.versionControl.length - 1 && ", "}
-                      </span>
-                    ))}
-                  </div>
-
-                  <div className={styles.selectedProjectTechWrapper}>
-                    <span>Testing: </span>
-
-                    {selectedProject.tech.development.testing.map((tech, i) => (
-                      <span key={tech} className={styles.selectedProjectTech}>
-                        {tech}
-                        {i < selectedProject.tech.development.testing.length - 1 && ", "}
-                      </span>
-                    ))}
-                  </div>
-
-                  <div className={styles.selectedProjectTechWrapper}>
-                    <span>Deployment: </span>
-
-                    {selectedProject.tech.development.deployment.map((tech, i) => (
-                      <span key={tech} className={styles.selectedProjectTech}>
-                        {tech}
-                        {i < selectedProject.tech.development.deployment.length - 1 && ", "}
-                      </span>
-                    ))}
-                  </div>
-                </>
-              )}
-            </div>
-          </div>
-
-          <p className={styles.selectedProjectDescription}>{selectedProject.description}</p>
-
-          <span className={styles.closeSelectedProject} onClick={() => setSelectedProject(null)}>
-            X
-          </span>
-        </div>
-      )}
+      {selectedProject && <SelectedProject selectedProject={selectedProject} setSelectedProject={setSelectedProject} />}
 
       {PROJECTS.map((project) => (
-        <SingleProject project={project} setSelectedProject={setSelectedProject} key={project.url} />
+        <SingleProject project={project} setSelectedProject={setSelectedProject} key={project.image} />
       ))}
 
       {selectedProject && <div className={styles.blurredOverlay} ref={blurredOverlayRef} />}
