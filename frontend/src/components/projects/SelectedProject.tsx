b206d98dddabe61a764847f0005cfd51645dc8ec
Valery <valeri.t.yanev@gmail.com>
2025-11-09 16:09:47 +0200

    renamed classNames and added github link to the selected project.

--- frontend/src/components/projects/SelectedProject.tsx
+++ frontend/src/components/projects/SelectedProject.tsx
@@ -13,29 +13,29 @@ const SelectedProject: React.FC<Props> = ({ selectedProject, setSelectedProject 
     <div className={styles.selectedProject}>
       <h3>{selectedProject.overview}</h3>
 
-      <div className={styles.selectedProjectMainPart}>
+      <div className={styles.mainPart}>
         <a href={selectedProject.url} target="_blank">
-          <video src={`/${selectedProject.video}`} className={styles.selectedProjectVideo} muted autoPlay loop />
+          <video src={`/${selectedProject.video}`} className={styles.video} muted autoPlay loop />
         </a>
 
-        <div className={styles.selectedProjectInformation}>
+        <div className={styles.information}>
           <p>Information:</p>
 
           {(selectedProject.tech.frontend.core.length > 0 ||
             selectedProject.tech.frontend.styling.length > 0 ||
             selectedProject.tech.frontend.tooling.length > 0) && (
-            <div className={styles.selectedProjectSingleTechWrapper}>
-              <p className={styles.selectedProjectFeBeDe}>Frontend:</p>
+            <div className={styles.singleTechWrapper}>
+              <p className={styles.feBeDe}>Frontend:</p>
 
               {selectedProject.tech.frontend.core.length > 0 && (
-                <div className={styles.selectedProjectTechWrapper}>
+                <div className={styles.techWrapper}>
                   <span>Core: </span>
 
                   {selectedProject.tech.frontend.core.map((tech, i) => (
                     <React.Fragment key={uuidv4()}>
-                      <img key={uuidv4()} src={tech.img} className={styles.selectedProjectTechImg} />
+                      <img key={uuidv4()} src={tech.img} className={styles.techImg} />
 
-                      <span key={uuidv4()} className={styles.selectedProjectTechText}>
+                      <span key={uuidv4()} className={styles.techText}>
                         {tech.text}
                         {i < selectedProject.tech.frontend.core.length - 1 && ", "}
                       </span>
@@ -45,14 +45,14 @@ const SelectedProject: React.FC<Props> = ({ selectedProject, setSelectedProject 
               )}
 
               {selectedProject.tech.frontend.styling.length && (
-                <div className={styles.selectedProjectTechWrapper}>
+                <div className={styles.techWrapper}>
                   <span>Styling: </span>
 
                   {selectedProject.tech.frontend.styling.map((tech, i) => (
                     <React.Fragment key={uuidv4()}>
-                      <img key={uuidv4()} src={tech.img} className={styles.selectedProjectTechImg} />
+                      <img key={uuidv4()} src={tech.img} className={styles.techImg} />
 
-                      <span key={uuidv4()} className={styles.selectedProjectTechText}>
+                      <span key={uuidv4()} className={styles.techText}>
                         {tech.text}
                         {i < selectedProject.tech.frontend.styling.length - 1 && ", "}
                       </span>
@@ -62,14 +62,14 @@ const SelectedProject: React.FC<Props> = ({ selectedProject, setSelectedProject 
               )}
 
               {selectedProject.tech.frontend.tooling.length > 0 && (
-                <div className={styles.selectedProjectTechWrapper}>
+                <div className={styles.techWrapper}>
                   <span>Tools: </span>
 
                   {selectedProject.tech.frontend.tooling.map((tech, i) => (
                     <React.Fragment key={uuidv4()}>
-                      <img key={uuidv4()} src={tech.img} className={styles.selectedProjectTechImg} />
+                      <img key={uuidv4()} src={tech.img} className={styles.techImg} />
 
-                      <span key={uuidv4()} className={styles.selectedProjectTechText}>
+                      <span key={uuidv4()} className={styles.techText}>
                         {tech.text}
                         {i < selectedProject.tech.frontend.tooling.length - 1 && ", "}
                       </span>
@@ -86,14 +86,14 @@ const SelectedProject: React.FC<Props> = ({ selectedProject, setSelectedProject 
                 <p className={styles.selectedProjectFeBeDe}>Backend:</p>
 
                 {selectedProject.tech.backend.server.length > 0 && (
-                  <div className={styles.selectedProjectTechWrapper}>
+                  <div className={styles.techWrapper}>
                     <span>Server: </span>
 
                     {selectedProject.tech.backend.server.map((tech, i) => (
                       <React.Fragment key={uuidv4()}>
-                        <img key={uuidv4()} src={tech.img} className={styles.selectedProjectTechImg} />
+                        <img key={uuidv4()} src={tech.img} className={styles.techImg} />
 
-                        <span key={uuidv4()} className={styles.selectedProjectTechText}>
+                        <span key={uuidv4()} className={styles.techText}>
                           {tech.text}
                           {selectedProject.tech.backend && i < selectedProject.tech.backend.server.length - 1 && ", "}
                         </span>
@@ -103,14 +103,14 @@ const SelectedProject: React.FC<Props> = ({ selectedProject, setSelectedProject 
                 )}
 
                 {selectedProject.tech.backend.database.length > 0 && (
-                  <div className={styles.selectedProjectTechWrapper}>
+                  <div className={styles.techWrapper}>
                     <span>Database: </span>
 
                     {selectedProject.tech.backend.database.map((tech, i) => (
                       <React.Fragment key={uuidv4()}>
-                        <img key={uuidv4()} src={tech.img} className={styles.selectedProjectTechImg} />
+                        <img key={uuidv4()} src={tech.img} className={styles.techImg} />
 
-                        <span key={uuidv4()} className={styles.selectedProjectTechText}>
+                        <span key={uuidv4()} className={styles.techText}>
                           {tech.text}
                           {selectedProject.tech.backend && i < selectedProject.tech.backend.database.length - 1 && ", "}
                         </span>
@@ -128,14 +128,14 @@ const SelectedProject: React.FC<Props> = ({ selectedProject, setSelectedProject 
               <p className={styles.selectedProjectFeBeDe}>Development:</p>
 
               {selectedProject.tech.development.versionControl.length > 0 && (
-                <div className={styles.selectedProjectTechWrapper}>
+                <div className={styles.techWrapper}>
                   <span>Version Control: </span>
 
                   {selectedProject.tech.development.versionControl.map((tech, i) => (
                     <React.Fragment key={uuidv4()}>
-                      <img key={uuidv4()} src={tech.img} className={styles.selectedProjectTechImg} />
+                      <img key={uuidv4()} src={tech.img} className={styles.techImg} />
 
-                      <span key={uuidv4()} className={styles.selectedProjectTechText}>
+                      <span key={uuidv4()} className={styles.techText}>
                         {tech.text}
                         {i < selectedProject.tech.development.versionControl.length - 1 && ", "}
                       </span>
@@ -145,14 +145,14 @@ const SelectedProject: React.FC<Props> = ({ selectedProject, setSelectedProject 
               )}
 
               {selectedProject.tech.development.testing.length > 0 && (
-                <div className={styles.selectedProjectTechWrapper}>
+                <div className={styles.techWrapper}>
                   <span>Testing: </span>
 
                   {selectedProject.tech.development.testing.map((tech, i) => (
                     <React.Fragment key={uuidv4()}>
-                      <img key={uuidv4()} src={tech.img} className={styles.selectedProjectTechImg} />
+                      <img key={uuidv4()} src={tech.img} className={styles.techImg} />
 
-                      <span key={uuidv4()} className={styles.selectedProjectTechText}>
+                      <span key={uuidv4()} className={styles.techText}>
                         {tech.text}
                         {i < selectedProject.tech.development.testing.length - 1 && ", "}
                       </span>
@@ -162,14 +162,14 @@ const SelectedProject: React.FC<Props> = ({ selectedProject, setSelectedProject 
               )}
 
               {selectedProject.tech.development.deployment.length > 0 && (
-                <div className={styles.selectedProjectTechWrapper}>
+                <div className={styles.techWrapper}>
                   <span>Deployment: </span>
 
                   {selectedProject.tech.development.deployment.map((tech, i) => (
                     <React.Fragment key={uuidv4()}>
-                      <img key={uuidv4()} src={tech.img} className={styles.selectedProjectTechImg} />
+                      <img key={uuidv4()} src={tech.img} className={styles.techImg} />
 
-                      <span key={uuidv4()} className={styles.selectedProjectTechText}>
+                      <span key={uuidv4()} className={styles.techText}>
                         {tech.text}
                         {i < selectedProject.tech.development.deployment.length - 1 && ", "}
                       </span>
@@ -182,7 +182,14 @@ const SelectedProject: React.FC<Props> = ({ selectedProject, setSelectedProject 
         </div>
       </div>
 
-      <p className={styles.selectedProjectDescription}>{selectedProject.description}</p>
+      <div className={styles.ghLink}>
+        <img src="svg/github.svg" alt="GitHub image." className={styles.img} />
+        <a className={styles.text} href={selectedProject.gh} target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </div>
+
+      <p className={styles.description}>{selectedProject.description}</p>
 
       <span className={styles.closeSelectedProject} onClick={() => setSelectedProject(null)}>
         X
