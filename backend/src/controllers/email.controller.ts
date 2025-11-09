d37dcc431453844098b02a45d2b0d15b847ef817
Valery <valeri.t.yanev@gmail.com>
2025-11-09 16:42:57 +0200

    added dompurify to sanitize the message input from frontend before sending it as an email.

--- backend/src/controllers/email.controller.ts
+++ backend/src/controllers/email.controller.ts
@@ -1,11 +1,25 @@
 import type { Request, Response } from "express";
 import { sendEmailService } from "../services/email.service.js";
+import { Email } from "custom-validation";
+import { sanitizeInput } from "../utils/sanitizer.js";
 
 export const sendEmail = async (req: Request, res: Response): Promise<void> => {
-  const { data } = req.body;
+  const { data }: { data: Email } = req.body;
+
+  const rawMessage = data.message;
+
+  const sanitizedMessage = sanitizeInput(rawMessage); // sanitize the message.
+
+  const sanitizedData: Email = {
+    botField: data.botField,
+    email: data.email,
+    loadTime: data.loadTime,
+    message: sanitizedMessage,
+    name: data.name,
+  };
 
   try {
-    sendEmailService(data);
+    sendEmailService(sanitizedData);
 
     res.status(201).json({ success: true });
     return;
