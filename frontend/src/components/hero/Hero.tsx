47b8c6ea83bddc3cefdba688de1a51ff58ffca8c
Valery <valeri.t.yanev@gmail.com>
2025-11-09 14:14:52 +0200

    removed the images of tech, decided to keep the tech stack in the project themselves

--- frontend/src/components/hero/Hero.tsx
+++ frontend/src/components/hero/Hero.tsx
@@ -4,25 +4,11 @@ import styles from "./Hero.module.scss";
 const Hero: React.FC = () => {
   return (
     <section className={styles.sectionHero}>
-      <h1>Valeri Yanev | Full-Stack Web Developer</h1>
-      <p className={styles.sectionHeroIntroduction}>
+      <h1 className={styles.heading}>Valeri Yanev | Full-Stack Web Developer</h1>
+      <p className={styles.introduction}>
         I build secure and fast web applications with React, TypeScript and Node.js, creating complex features like
         secure payment integration and real-time data tracking.
       </p>
-
-      <ul className={styles.sectionHeroMainTechStack}>
-        <li>
-          <img src="/svg/typescript.svg" />
-        </li>
-        <li>
-          <img src="/svg/react.svg" />
-        </li>
-        <li>
-          <img src="/svg/nodejs.svg" />
-        </li>
-      </ul>
-
-      <div></div>
     </section>
   );
 };
