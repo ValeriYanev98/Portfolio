deafaaf4c315c5b7b5174b0e54a5be42e4eadadb
Valery <valeri.t.yanev@gmail.com>
2025-11-09 22:54:30 +0200

    added scroll to top if the user scrolls below the projects

--- frontend/src/components/hero/Hero.tsx
+++ frontend/src/components/hero/Hero.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./Hero.module.scss";
+import { useInView } from "react-intersection-observer";
 
-const Hero: React.FC = () => {
+interface Props {
+  setShowScrollToTop: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Hero: React.FC<Props> = ({ setShowScrollToTop }) => {
+  const { ref, inView = undefined } = useInView({
+    threshold: 1,
+  });
+
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+
+    if (!inView) {
+      setShowScrollToTop(true);
+    } else {
+      setShowScrollToTop(false);
+    }
+  }, [inView]);
+
   return (
-    <section className={styles.sectionHero} id="hero">
+    <section className={styles.sectionHero} id="hero" ref={ref}>
       <h1 className={styles.heading}>Valeri Yanev | Full-Stack Web Developer</h1>
       <p className={styles.introduction}>
         I build secure and fast web applications with React, TypeScript and Node.js, creating complex features like
