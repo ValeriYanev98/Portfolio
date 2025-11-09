aed438d2d293d9aef88fac744022f210b096cf60
Valery <valeri.t.yanev@gmail.com>
2025-11-09 15:35:14 +0200

    changed where the errors would appear

--- frontend/src/components/contact/Contact.tsx
+++ frontend/src/components/contact/Contact.tsx
@@ -36,30 +36,56 @@ const Contact: React.FC = () => {
 
   return (
     <section className={styles.sectionContact}>
-      <h3>Get in Touch</h3>
-      <p>Feel free to reach out - I'm open to work!</p>
+      <h3 className={styles.heading}>Get in Touch</h3>
+      <p className={styles.secondaryText}>Feel free to reach out - I'm open to work!</p>
 
-      <form onSubmit={handleSubmit(sendEmail)}>
-        <div className={styles.sectionContactRowWrapper}>
-          <label>Name: </label>
-          <input {...register("name")} type="text" placeholder="Name..." />
-          {errors && errors.name && <span>{errors.name.message}</span>}
+      <form onSubmit={handleSubmit(sendEmail)} className={styles.form}>
+        <div className={styles.rowWrapper}>
+          <div>
+            <label className={styles.label} htmlFor="name">
+              Name:
+            </label>
+
+            {errors && errors.name && <span className={styles.error}>{errors.name.message}</span>}
+          </div>
+          <input {...register("name")} type="text" placeholder="Name..." className={styles.input} id="name" />
         </div>
-        <div className={styles.sectionContactRowWrapper}>
-          <label>Email: </label>
-          <input {...register("email")} type="email" placeholder="Email..." />
-          {errors && errors.email && <span>{errors.email.message}</span>}
+        <div className={styles.rowWrapper}>
+          <div>
+            <label className={styles.label} htmlFor="email">
+              Email:
+            </label>
+
+            {errors && errors.email && <span className={styles.error}>{errors.email.message}</span>}
+          </div>
+
+          <input {...register("email")} type="email" placeholder="Email..." className={styles.input} id="email" />
         </div>
-        <div className={styles.sectionContactRowWrapper}>
-          <label>Message: </label>
-          <textarea {...register("message")} cols={50} rows={8} placeholder="Message..." />
-          {errors && errors.message && <span>{errors.message.message}</span>}
+        <div className={styles.rowWrapper}>
+          <div>
+            <label className={styles.label} htmlFor="message">
+              Message:
+            </label>
+
+            {errors && errors.message && <span className={styles.error}>{errors.message.message}</span>}
+          </div>
+
+          <textarea
+            {...register("message")}
+            cols={50}
+            rows={8}
+            placeholder="Message..."
+            className={styles.input}
+            id="message"
+          />
         </div>
 
         <input {...register("botField")} autoComplete="off" style={{ display: "none" }} tabIndex={-1} />
         <input type="hidden" {...register("loadTime", { valueAsNumber: true })} />
 
-        <button type="submit">Send Email</button>
+        <button type="submit" className={styles.button}>
+          Send Email
+        </button>
       </form>
     </section>
   );
