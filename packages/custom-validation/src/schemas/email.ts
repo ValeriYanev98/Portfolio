af0c2a8dd5bb6e4dc82dd490da4c48e770bd3b7d
Valery <valeri.t.yanev@gmail.com>
2025-11-10 19:07:17 +0200

    changed the min length

--- packages/custom-validation/src/schemas/email.ts
+++ packages/custom-validation/src/schemas/email.ts
@@ -9,7 +9,7 @@ export const EmailSchema = z.object({
   email: z.email({ error: "Not an email!" }),
   message: z
     .string({ error: "Not a string" })
-    .min(20, { error: "Message too short, expected at least 20 characters." })
+    .min(10, { error: "Message too short." })
     .max(1000, { error: "Message too long!" }),
   botField: z.string().max(0, { error: "Nice try, BOT!" }), // honeypot check if there is bot trying to spam, he will try to fill all fields.
   loadTime: z.number().refine((val) => Date.now() - val > 3000), // if the form was submitted too fast, like less than 3 seconds, def a bot, since the textarea doesnt have autofill option.
