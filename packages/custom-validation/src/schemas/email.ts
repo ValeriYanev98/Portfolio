5ea80c65b608aefe4ec73fc82ed2d84233e44599
Valery <valeri.t.yanev@gmail.com>
2025-11-11 12:23:26 +0200

    changed to less restrictive schema.

--- packages/custom-validation/src/schemas/email.ts
+++ packages/custom-validation/src/schemas/email.ts
@@ -1,11 +1,7 @@
 import * as z from "zod";
 
 export const EmailSchema = z.object({
-  name: z
-    .string({ error: "Not a string!" })
-    .regex(/^[\x20-\x7A]*$/, { error: "Only normal characters!" })
-    .min(2, { error: "Name too short!" })
-    .max(122, { error: "Name too long!" }),
+  name: z.string({ error: "Not a string!" }).min(2, { error: "Name too short!" }).max(122, { error: "Name too long!" }),
   email: z.email({ error: "Not an email!" }),
   message: z
     .string({ error: "Not a string" })
